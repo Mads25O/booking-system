8f72d31acacf1987f2db8e655410b123e3fa2714
Mads Overgård <mo.overgaard@live.dk>
2024-12-14 13:30:15 +0100

    Sidste commit til azure_vm

--- website/__init__.py
+++ website/__init__.py
@@ -31,6 +31,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    login_manager.login_message = "Du skal være logget ind for at få adgang til denne side."
 
 
     @login_manager.user_loader
